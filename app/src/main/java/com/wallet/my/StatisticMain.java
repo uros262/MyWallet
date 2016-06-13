941d6c383c499467e48d17f2361aa321d0cd3182
Uros Gogic <uros.gogic93@yahoo.com>
2016-06-13 23:50:10 +0200

    restore backup

--- app/src/main/java/com/wallet/my/StatisticMain.java
+++ app/src/main/java/com/wallet/my/StatisticMain.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.wallet.my.DB.BalanceDbHelper;
 import com.wallet.my.DB.ExpenseDbHelper;
@@ -31,23 +33,28 @@ import cz.msebera.android.httpclient.Header;
 
 public class StatisticMain extends AppCompatActivity implements View.OnClickListener {
 
-    Button backup;
     Button incomes;
     Button expenses;
+    Button backup;
+    Button restoreBackup;
     BalanceDbHelper balanceDbHelper;
     InPocketDbHelper inPocketDbHelper;
     IncomeDbHelper incomeDbHelper;
     ExpenseDbHelper expenseDbHelper;
 
     private void Init(){
-        backup = (Button) findViewById(R.id.btnBackup);
-        backup.setOnClickListener(this);
 
         incomes = (Button) findViewById(R.id.btnStatisticIncomes);
         incomes.setOnClickListener(this);
 
         expenses = (Button) findViewById(R.id.btnStatisticExpenses);
         expenses.setOnClickListener(this);
+
+        backup = (Button) findViewById(R.id.btnBackup);
+        backup.setOnClickListener(this);
+
+        restoreBackup = (Button) findViewById(R.id.btnRestoreBackup);
+        restoreBackup.setOnClickListener(this);
 
         balanceDbHelper = new BalanceDbHelper(this);
         inPocketDbHelper = new InPocketDbHelper(this);
@@ -108,6 +115,29 @@ public class StatisticMain extends AppCompatActivity implements View.OnClickList
                         @Override
                         public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                             Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                catch(Exception ex)
+                {
+//                    ex.getMessage();
+                }
+                break;
+
+            case R.id.btnRestoreBackup:
+
+                try {
+
+                    //Create AsycHttpClient object
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    //http://loopj.com/android-async-http/
+                    client.get("http://192.168.1.67/insertuser.php?giveMeBackup=true", new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                            // If the response is JSONObject instead of expected JSONArray
+                            Log.d("Rezultat", response.toString());
+                            //https://developer.android.com/reference/org/json/JSONObject.html
+
                         }
                     });
                 }
